c11fb21603be2999b185543d4d340605bb21af12
Tanmay Goel <goeltanmay@gmail.com>
2014-07-25 16:09:30 +0530

    request.GET

--- IVR/views.py
+++ IVR/views.py
@@ -7,9 +7,10 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
 
 def greeting_view(request):
-	callSid = request.QUERY_PARAMS["CallSid"]
-	frm = request.QUERY_PARAMS["From"]
-	to = request.QUERY_PARAMS["To"]
+	callSid = request.GET["CallSid"]
+	frm = request.GET["From"]
+	to = request.GET["To"]
+
 	response = HttpResponse("https://s3.amazonaws.com/digitalgreen/try2+(1).wav",content_type="text/plain")
 	response["CallSid"] = callSid
 	response["From"] = frm
