3053039349d2c0dbc7a18abe9638c87d9f46a5e0
Tanmay Goel <goeltanmay@gmail.com>
2014-07-25 16:02:20 +0530

    changing views

--- IVR/views.py
+++ IVR/views.py
@@ -7,4 +7,14 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
 
 def greeting_view(request):
-	return HttpResponse("https://s3.amazonaws.com/digitalgreen/try2+(1).wav")
+	callSid = request.QUERY_PARAMS["CallSid"]
+	frm = request.QUERY_PARAMS["From"]
+	to = request.QUERY_PARAMS["To"]
+	response = HttpResponse("https://s3.amazonaws.com/digitalgreen/try2+(1).wav",content_type="text/plain")
+	response["CallSid"] = callSid
+	response["From"] = frm
+	response["To"] = to
+	response["DialWhomNumber"] = ""
+
+
+	return response
